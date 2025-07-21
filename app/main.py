c3f77c3bf4136b41ea00f5b2f8a0a4f7acf8d3ed
HarryPotter0107 <bhanuupadhyay87@gmail.com>
2025-07-21 10:34:50 +0530

    root endpoint added

--- app/main.py
+++ app/main.py
@@ -3,5 +3,10 @@ from app.routes import product, order
 
 app = FastAPI(title="HROne API")
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to HROne API!"}
+
+# Register API routers
 app.include_router(product.router, prefix="/products", tags=["Products"])
 app.include_router(order.router, prefix="/orders", tags=["Orders"])
